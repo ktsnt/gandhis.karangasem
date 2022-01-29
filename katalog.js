eaca4711d3a440e27b24a90cef6f9b3f013fc9a1
ktsnt <91922301+ktsnt@users.noreply.github.com>
2022-01-29 19:21:24 +0800

    Update katalog.js

--- katalog.js
+++ katalog.js
@@ -1,26 +1,33 @@
 $(document).ready(function() {
   $('.card-slider').slick({
-    dots: false,
-    arrows: true,
+    dots: true,
+    speed: 300,
     slidesToShow: 4,
+    slidesToScroll: 4,
     infinite: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 4
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
         }
       },
       {
         breakpoint: 800,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 3,
+          slidesToScroll: 3
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true
         }
       }
     ]
