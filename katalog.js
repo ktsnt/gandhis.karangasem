6ee4dfb1c78e12fd9d13293889aad9599c25f73d
ktsnt <91922301+ktsnt@users.noreply.github.com>
2022-01-29 19:27:48 +0800

    Update katalog.js

--- katalog.js
+++ katalog.js
@@ -3,14 +3,12 @@ $(document).ready(function() {
     dots: true,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
     infinite: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
         infinite: true,
         dots: true
         }
@@ -19,14 +17,12 @@ $(document).ready(function() {
         breakpoint: 800,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
           dots: true
         }
       }
