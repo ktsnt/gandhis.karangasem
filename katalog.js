b0abeb44e16bf445668b5962253416f4edca6c7a
ktsnt <91922301+ktsnt@users.noreply.github.com>
2022-01-29 19:34:49 +0800

    Update katalog.js

--- katalog.js
+++ katalog.js
@@ -1,29 +1,26 @@
 $(document).ready(function() {
   $('.card-slider').slick({
     dots: true,
-    speed: 300,
+    arrows: true,
     slidesToShow: 4,
     infinite: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-        slidesToShow: 3,
-        infinite: true,
-        dots: true
+          slidesToShow: 4
         }
       },
       {
         breakpoint: 800,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 3
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          dots: true
+          slidesToShow: 1
         }
       }
     ]
